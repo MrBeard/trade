acb2bd4ad525e3286fc390a7ac719856792c4ba4
MrBeard <Mr.Beard.mail@gmail.com>
2017-06-08 18:51:30 +0300

    init

--- src/main/scala/org/test/trade/Matcher.scala
+++ src/main/scala/org/test/trade/Matcher.scala
@@ -14,32 +14,33 @@ object Matcher {
         case s: Sell => handleSell(s)
         case b: Buy => handleBuy(b)
       }
+    }
 
-      def handleSell(s: Sell) = {
-        val offer = s.offer
-        val buyBid = toBuy(offer).find(_.name != s.name)
-        if (buyBid.isEmpty)
-          toSell(offer) = toSell(offer).enqueue(s)
-        else {
-          val bb = buyBid.get
-          if (ClientStorage.applyOffer(s.name, bb.name, offer))
-            toBuy(offer) = toBuy(offer) diff Queue(bb)
-        }
-      }
-
-      def handleBuy(b: Buy) = {
-        val offer = b.offer
-        val sellBid = toSell(offer).find(_.name != b.name)
-        if (sellBid.isEmpty)
-          toBuy(offer) = toBuy(offer).enqueue(b)
-        else {
-          val sb = sellBid.get
-          if (ClientStorage.applyOffer(sb.name, b.name, offer))
-            toSell(offer) = toSell(offer) diff Queue(sb)
-        }
-
+    def handleSell(s: Sell) = {
+      val offer = s.offer
+      val buyBid = toBuy(offer).find(_.name != s.name)
+      if (buyBid.isEmpty)
+        toSell(offer) = toSell(offer).enqueue(s)
+      else {
+        val bb = buyBid.get
+        if (ClientStorage.applyOffer(s.name, bb.name, offer))
+          toBuy(offer) = toBuy(offer) diff Queue(bb)
       }
     }
+
+    def handleBuy(b: Buy) = {
+      val offer = b.offer
+      val sellBid = toSell(offer).find(_.name != b.name)
+      if (sellBid.isEmpty)
+        toBuy(offer) = toBuy(offer).enqueue(b)
+      else {
+        val sb = sellBid.get
+        if (ClientStorage.applyOffer(sb.name, b.name, offer))
+          toSell(offer) = toSell(offer) diff Queue(sb)
+      }
+
+    }
+
   }
 
 }
